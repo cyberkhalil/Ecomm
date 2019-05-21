f079f350fcc2e31d7b7947f88f79ed70f794c409
Mahmoud Khalil <khalil2535@protonmail.com>
2019-05-21 09:04:18 +0300

    apply .gitignore file

--- Ecommerce/Ecommerce/Admin/AddProduct.aspx.cs
+++ Ecommerce/Ecommerce/Admin/AddProduct.aspx.cs
@@ -4,10 +4,7 @@ namespace Ecommerce.Admin
 {
     public partial class AddProduct : System.Web.UI.Page
     {
-        protected void Page_Load(object sender, EventArgs e)
-        {
-
-        }
+        protected void Page_Load(object sender, EventArgs e) { }
 
         protected void Button2_Click(object sender, EventArgs e)
         {
@@ -18,7 +15,6 @@ namespace Ecommerce.Admin
         {
             TextBoxCalender.Text = Calendar1.SelectedDate.ToLongDateString();
             Calendar1.Visible = false;
-
         }
 
         protected void Submit_Click(object sender, EventArgs e)
@@ -26,26 +22,16 @@ namespace Ecommerce.Admin
             // TODO maybe timedate instead of time & calender should be shown with hours and mins
 
             if (!FileUpload1.HasFile)
-            {
-                LabelinsertPro.Text = "إختر ملف لرفعه";
-                return;
-            }
+            { LabelinsertPro.Text = "إختر ملف لرفعه"; return; }
 
             string strRealPath = Request.PhysicalApplicationPath + "Assets\\images\\products\\";
 
             if (!System.IO.Directory.Exists(strRealPath))
-            {
-                LabelinsertPro.Text = "المجلد الذي سيحمل فيه الملفات غير موجود على الخادم";
-                return;
-            }
+            { LabelinsertPro.Text = "المجلد الذي سيحمل فيه الملفات غير موجود على الخادم"; return; }
             if (System.IO.File.Exists(strRealPath + FileUpload1.FileName))
-            {
-                LabelinsertPro.Text = "هذا الملف موجود حاول تغيير إسم الملف";
-            }
+            { LabelinsertPro.Text = "هذا الملف موجود حاول تغيير إسم الملف"; return; }
             if (FileUpload1.PostedFile.ContentLength >= 15000)
-            {
-                LabelinsertPro.Text = "اختر صورة بحجم أصغر";
-            }
+            { LabelinsertPro.Text = "اختر صورة بحجم أصغر"; return; }
 
             string ProductImgFile = strRealPath + FileUpload1.FileName;
             FileUpload1.SaveAs(strRealPath + FileUpload1.FileName);
@@ -56,10 +42,7 @@ namespace Ecommerce.Admin
                 int rowsAffected1 = SqlDataSource1.Insert();
                 LabelinsertPro.Text = "تم الإضافة بنجاح";
             }
-            catch (Exception ex)
-            {
-                LabelinsertPro.Text = ex.Message;
-            }
+            catch (Exception ex) { LabelinsertPro.Text = ex.Message; }
         }
     }
 }
